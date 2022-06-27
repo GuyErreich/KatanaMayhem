d81cc10e51c8b3d2df860f7a92a4195fc137ec40
Guy Erreich <guyerreich@gmail.com>
2022-06-28 00:14:46 +0300

    Shoot turns into blocks at hit

--- Assets/KatanaMayhem/Scripts/ColliderHit.cs
+++ Assets/KatanaMayhem/Scripts/ColliderHit.cs
@@ -3,15 +3,19 @@ using UnityEngine;
 namespace KatanaMayhem.Scripts
 {
     public class ColliderHit : MonoBehaviour {
+        
+        [SerializeField] private GameObject staticSlime;
 
         Rigidbody rb;
 
         private void Awake() {
             this.rb = this.GetComponent<Rigidbody>();
         }
-        
+
         private void OnCollisionEnter(Collision other) {
-            this.rb.isKinematic = true;
+            var rotation = Quaternion.FromToRotation(Vector3.forward, other.GetContact(0).normal);
+            Instantiate(staticSlime, other.GetContact(0).point, rotation);
+            Destroy(this.gameObject);
         }
     }
 }
